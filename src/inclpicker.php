79befbdd81833c3a40c06dbca9c42c200c5571f0
AntoineDannemark <antoine.dannemark@gmail.com>
2018-12-03 18:21:05 +0100

    picker panel @work

--- src/inclpicker.php
+++ src/inclpicker.php
@@ -1,6 +1,30 @@
 <?php
-
-    include 'picker.php';
-
-
+session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Inclpickertest</title>
+</head>
+<body>
+    <div class="container-fluid">
+       
+        <div class="col-3 grey">
+            <br>
+            <?php
+                include 'picker.php';
+            ?>
+            <br>
+        </div>
+
+    </div>
+
+    
+</body>
+</html>
--- src/picker.php
+++ src/picker.php
@@ -13,7 +13,7 @@ try {
     $db = new PDO ($dsn, $user_db, $pass_db);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $q_user="SELECT id, pseudo FROM user";    
+    $q_user="SELECT id, pseudo, picture FROM user";    
     $result = $db->query($q_user)->fetchAll(PDO::FETCH_ASSOC);
    
     $id_arr = array('<br>');
@@ -27,11 +27,9 @@ try {
             array_push($id_arr, $row['id'] . '<br>');
             array_push($psd_arr, $row['pseudo'] . '<br>');
             if($row['id']<$cur_mem_id){
-                $particip_id = $row['id']. " " . $cur_mem_id;
-                echo $particip_id . '<br>';
+                $particip_id = $row['id']. " " . $cur_mem_id;                
             }else{
-                $particip_id = $cur_mem_id . " " . $row['id'];
-                echo $particip_id . '<br>';
+                $particip_id = $cur_mem_id . " " . $row['id'];                
             }
             $q_conv_reg="SELECT id, particip_id FROM conv_reg WHERE particip_id = '".$particip_id."'";
             $res2 = $db->query($q_conv_reg)->fetchAll();
@@ -40,20 +38,22 @@ try {
             $q_msg="SELECT id, conv_reg_id, content FROM messages WHERE conv_reg_id = '".$res2."' ORDER BY id DESC LIMIT 1";
             $res3 = $db->query($q_msg)->fetchAll();
             array_push($last_msg_arr, $res3[0][2] . '<br>');
+            
+            if($row['picture']){
+                print "<div class='row'><img class='pro_pic' src='" . $row['picture'] . "'alt='profile picture'>";
+            }else{
+                print "<div class='row'><img class='pro_pic' src='uploads/def_icon.png'alt='profile picture'>";
+            } 
+
+            
+            if($res3[0][2]) { 
+                print "<div class='col-sm lightgrey'><strong> ". $row['pseudo'] . "</strong><br>" . $res3[0][2] . " </div></div><br>";
+            } else {
+                print "<div class='col-sm lightgrey'><strong> ". $row['pseudo'] . "</strong><br> Start new conversation </div></div><br>";
+            }
         }
     }
-    
 
-    print_r ($id_arr);
-    echo '<br>';
-    echo '<br>';
-    print_r ($psd_arr);
-    echo '<br>';
-    echo '<br>';
-    print_r ($conv_reg_arr);
-    echo '<br>';
-    echo '<br>';
-    print_r ($last_msg_arr);
 
     
 
