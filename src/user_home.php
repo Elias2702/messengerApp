ffc746d7156ce53ab0e3647236495a536a354537
tajahmedbecode <taj.alsir81@gmail.com>
2018-12-07 09:26:55 +0100

    before starting (add member to conversation , leave conversation

--- src/user_home.php
+++ src/user_home.php
@@ -30,16 +30,11 @@ require_once './func php/f_dsp_element.php';
     ?>
 
        <?php           
-<<<<<<< HEAD
-                $result = get_contacts();
-                                
-=======
             try {
                 $db_user = new PDO ($dsn, $user_db, $pass_db);
                 $db_user -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $q_user="SELECT id, prenom, nom, sexe, pseudo, email, pass FROM messenger.user;";
                 $result = $db_user -> query("$q_user")->fetchAll();      
->>>>>>> devantoine
         ?>
                 
                 <form action="send_new.php" name="main_form" method="POST" id="send" target="message_display_iframe" >
@@ -48,30 +43,14 @@ require_once './func php/f_dsp_element.php';
                     <legend><p>List of Members</p>
                     <select size="10" name="mem_sel" style="width: 250px;">
                         <?php foreach ($result as $row) {
-<<<<<<< HEAD
-                            dsp_element('option' , $row["pseudo"]);
-                                //echo "<option>". $row["pseudo"]. "<br> "."</option>";   //"<br> id: "." - Name: ". $row["prenom"]. "   " . $row["nom"] . " ". $row["sexe"] . " ". $row["pseudo"] . " ". $row["email"] . " ". $row["pass"] .
-                        } ;?>
-                    </select>
-                    </legend>
-        <?php           
-                //$result2 = get_convers_reg();
-                                
-        ?>
-                    <legend><p>list of conversations</p>
-                    <select size='10' name='conv_dsp' style='width: 250px;' onClick="">
-                    <option><table><tr><th><td>id </td><td>| prenom |</td><td>nom </td></th></tr></table></option>
-                        <?php foreach ($result2 as $row) {
-                            dsp_element('option' , $row['id']);                                      
-                                   // echo '<option>'.'<br>'.  $row['id'] .'<br>'.'</option>'; //'<br>'. 'id: '. $row['id']. ' - number of participants: '. $row['num_particip']. '   '. ' - creation time: '. $row['creation_time'] . ' - id of participants: '. $row['particip_id'] .
-                                    } ?>
-=======
                             if($row['id'] !== $cur_mem) {
                                 echo "<option value='" . $row['id'] . "' label='" .$row['pseudo']. "'> ". $row['pseudo'] ."</option>";     
                             }
                         }
+                    }catch (Exception $ex) {
+                        echo 'ERROR DBASE CONNECTION '.$ex->getMessage();
+                    } 
                         ?>
->>>>>>> devantoine
                     </select>
                     </legend>
                     </div>
@@ -79,32 +58,13 @@ require_once './func php/f_dsp_element.php';
         <?php                
                                   
         ?>
-<<<<<<< HEAD
-                    <p><?php //echo $_POST['member_sel'] ;?></p>
-=======
                     <p></p>
->>>>>>> devantoine
                                                 
                         <div><input type='text' name='crt_msg' id='crt_msg'></div>
                         <br>
                         <div><button type="submit" value="SEND">SEND</button></div>
                         <br>        
                         <div><iframe name="message_display_iframe" style="height:250px;">
-<<<<<<< HEAD
-                            
-                        </iframe>
-                        </div>
-                        
-                        <div><button type='button' name='display conversation' id='dsp_cnv' value='' action="display_conv.php" style="visibility:hidden">display conversation</button></div-->
-                        <br>
-                        <button> <a href="logout.php">Logout</a></button>
-                        <p><?php /* echo $_POST['member_sel'];// select the entire row,and then filter the name and id, and store them in variables*/?></p>
-
-<!--INSERT INTO `conv_reg` (`id`, `num_particip`, `creation_time`, `particip_id`) VALUES ('4', '3', '2018-11-07 00:00:00', '5');  -->                          
-                </form>
-
-           <script src="./script_taj.js"></script>    
-=======
                             <!-- <span>
                                 <select size='10' id='msg_dsp' style="width: 250px;">
                    
@@ -119,6 +79,5 @@ require_once './func php/f_dsp_element.php';
                 <br>
                 <button onclick="location.href='includer.php'">Inclpicker</button>
                 <br>
->>>>>>> devantoine
     </body>
 </html>
